714970a8607f7d4cb90ee4cbdaf58d8e1fd6191a
hhrum <denis.smit72@gmail.com>
2020-08-01 11:27:14 +0500

    Fix some design bags

--- application/lib/Smarty/template_c/c33626ab78a8d41c0cbb692af3637fb600297f67_0.file.index.tpl.php
+++ application/lib/Smarty/template_c/c33626ab78a8d41c0cbb692af3637fb600297f67_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-08-01 02:05:00
+/* Smarty version 3.1.34-dev-7, created on 2020-08-01 02:31:16
   from 'F:\IT\xampp\htdocs\php-shop\application\views\main\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5f24b1ac91e5d0_12359769',
+  'unifunc' => 'content_5f24b7d44fd949_76043222',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c33626ab78a8d41c0cbb692af3637fb600297f67' => 
     array (
       0 => 'F:\\IT\\xampp\\htdocs\\php-shop\\application\\views\\main\\index.tpl',
-      1 => 1596239758,
+      1 => 1596241874,
       2 => 'file',
     ),
   ),
@@ -20,6 +20,30 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5f24b1ac91e5d0_12359769 (Smarty_Internal_Template $_smarty_tpl) {
-?>Jopa<?php }
+function content_5f24b7d44fd949_76043222 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="hot-news">
+    <div class="hot-new__title">Title</div>
+    <div class="hot-news__description">Description of news</div>
+    <div class="hot-news__nav">
+        <div class="hot-news_nav-item hot-news_nav-item_selected"></div>
+        <div class="hot-news_nav-item"></div>
+        <div class="hot-news_nav-item"></div>
+    </div>
+</div>
+
+<div class="categories">
+    <div class="categories__header">
+        <div class="categories__title">Категории</div>
+        <div class="categories__show-all">Смотреть все</div>
+    </div>
+
+    <div class="categories__main">
+        <div class="category">
+            <div class="category__icon">
+                <i class="fa fa-desktop fa-2x" aria-hidden="true"></i>
+            </div>
+            <div class="category__title">ПК</div>
+        </div>
+    </div>
+</div><?php }
 }
--- application/views/main/index.tpl
+++ application/views/main/index.tpl
@@ -1,1 +1,25 @@
-Jopa
+<div class="hot-news">
+    <div class="hot-new__title">Title</div>
+    <div class="hot-news__description">Description of news</div>
+    <div class="hot-news__nav">
+        <div class="hot-news_nav-item hot-news_nav-item_selected"></div>
+        <div class="hot-news_nav-item"></div>
+        <div class="hot-news_nav-item"></div>
+    </div>
+</div>
+
+<div class="categories">
+    <div class="categories__header">
+        <div class="categories__title">Категории</div>
+        <div class="categories__show-all">Смотреть все</div>
+    </div>
+
+    <div class="categories__main">
+        <div class="category">
+            <div class="category__icon">
+                <i class="fa fa-desktop fa-2x" aria-hidden="true"></i>
+            </div>
+            <div class="category__title">ПК</div>
+        </div>
+    </div>
+</div>
