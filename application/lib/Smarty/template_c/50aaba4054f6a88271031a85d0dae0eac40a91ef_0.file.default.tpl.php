95b589921c12dcbccb25c2bdb861cb9379b5e64d
hhrum <denis.smit72@gmail.com>
2020-08-05 11:55:16 +0500

    Realized design

--- application/lib/Smarty/template_c/50aaba4054f6a88271031a85d0dae0eac40a91ef_0.file.default.tpl.php
+++ application/lib/Smarty/template_c/50aaba4054f6a88271031a85d0dae0eac40a91ef_0.file.default.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-08-04 20:54:23
+/* Smarty version 3.1.34-dev-7, created on 2020-08-05 08:53:49
   from 'C:\IT\xampp\htdocs\php-shop\application\views\layouts\default.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5f29aedf0529d6_86992420',
+  'unifunc' => 'content_5f2a577de62785_56047062',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '50aaba4054f6a88271031a85d0dae0eac40a91ef' => 
     array (
       0 => 'C:\\IT\\xampp\\htdocs\\php-shop\\application\\views\\layouts\\default.tpl',
-      1 => 1596567072,
+      1 => 1596610428,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5f29aedf0529d6_86992420 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5f2a577de62785_56047062 (Smarty_Internal_Template $_smarty_tpl) {
 ?></html><!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -40,7 +40,7 @@ function content_5f29aedf0529d6_86992420 (Smarty_Internal_Template $_smarty_tpl)
         <div class="row justify-content-center">
             <div class="col-12 col-md-4 col-lg-2 order-1">
                 <a href="<?php echo $_smarty_tpl->tpl_vars['url']->value;?>
-" class="header__logo">
+" class="header__logo header-link">
                     <i class="header__icon fa fa-slack fa-2x" aria-hidden="true"></i>
                     <p>Мой магазинчик</p>
                 </a>
@@ -48,7 +48,7 @@ function content_5f29aedf0529d6_86992420 (Smarty_Internal_Template $_smarty_tpl)
             <div class="col-12 col-md-6 col-lg-2 order-3 order-lg-2">
                 <div class="d-none d-md-flex header__categories dropdown">
                     <i class="header__icon fa fa-bars" aria-hidden="true"></i>
-                    <a href="#" class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a href="#" class="dropdown-toggle header-link" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Категории
                     </a>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
@@ -60,7 +60,7 @@ if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['category']->valu
 $_smarty_tpl->tpl_vars['category']->do_else = false;
 ?>
 
-                        <a class="dropdown-item" href="<?php echo $_smarty_tpl->tpl_vars['url']->value;?>
+                        <a class="dropdown-item header-link" href="<?php echo $_smarty_tpl->tpl_vars['url']->value;?>
 /main/category/?category_id=<?php echo $_smarty_tpl->tpl_vars['category']->value['id'];?>
 ">
                             <i class="fa fa-<?php echo $_smarty_tpl->tpl_vars['category']->value['icon'];?>
@@ -75,7 +75,7 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                 </div>
                 <div class="d-block d-md-none header__categories dropdown">
                     <i class="header__icon fa fa-bars" aria-hidden="true"></i>
-                    <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <a class="header-link" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                         Категории
                     </a>
                 </div>
@@ -88,10 +88,19 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                 </div>
             </div>
             <div class="d-none d-md-flex col-2 col-m-3 col-md-2 order-4">
-                <div class="header__contacts">Контакты</div>
+                <div class="header__order">
+                    <i class="header__icon fa fa-shopping-cart" aria-hidden="true">
+                        <span class="header__order-count">2</span>
+                    </i>
+                    <a href="#" class="header-link">Корзина</a>
+                </div>
             </div>
             <div class="d-none d-md-flex col-2 col-m-3 col-md-2 order-5">
-                <div class="header__help">Помощь</div>
+                <div class="header__account">
+                    <a href="#" class="standart-btn" data-toggle="modal" data-target="#signinModal">
+                        Войти
+                    </a>
+                </div>
             </div>
         </div>
         <div class="row">
@@ -117,14 +126,6 @@ $_smarty_tpl->tpl_vars['category']->do_else = false;
                     <?php
 }
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
-                    
-                    <hr class="header__categories-collapse-seperator">
-                    <a class="dropdown-item header__categories-collapse-item" href="#">
-                        Контакты
-                    </a>
-                    <a class="dropdown-item header__categories-collapse-item" href="#">
-                        Помощь
-                    </a>
                 </div>
             </div>
         </div>
@@ -136,6 +137,54 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     <footer class="container">
         Footer
     </footer>
+
+    <div class="modal fade" id="signinModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm">
+          <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Авторизация</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="modal__form">
+                    <input type="text" name="email" placeholder="Введите ваш Email">
+                    <input type="password" name="password" placeholder="Введите ваш пароль">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="modal-btn standart-btn">Войти</a>
+                <a href="#" class="login-modal__signup" data-dismiss="modal"  data-toggle="modal" data-target="#signupModal">Зарегистрироваться</a>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm">
+          <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Регистрация</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="modal__form">
+                    <input type="text" name="name" placeholder="Имя">
+                    <input type="text" name="email" placeholder="Введите ваш Email">
+                    <input type="password" name="password1" placeholder="Пароль">
+                    <input type="password" name="password2" placeholder="Пароль повторно">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="modal-btn standart-btn">Создать аккаунт</a>
+            </div>
+          </div>
+        </div>
+    </div>
+    
     <?php echo '<script'; ?>
  src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"><?php echo '</script'; ?>
 >
--- application/views/layouts/default.tpl
+++ application/views/layouts/default.tpl
@@ -13,7 +13,7 @@
     <div class="header container">
         <div class="row justify-content-center">
             <div class="col-12 col-md-4 col-lg-2 order-1">
-                <a href="{$url}" class="header__logo">
+                <a href="{$url}" class="header__logo header-link">
                     <i class="header__icon fa fa-slack fa-2x" aria-hidden="true"></i>
                     <p>Мой магазинчик</p>
                 </a>
@@ -21,13 +21,13 @@
             <div class="col-12 col-md-6 col-lg-2 order-3 order-lg-2">
                 <div class="d-none d-md-flex header__categories dropdown">
                     <i class="header__icon fa fa-bars" aria-hidden="true"></i>
-                    <a href="#" class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a href="#" class="dropdown-toggle header-link" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Категории
                     </a>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                         {foreach from=$categories item=category name=categories}
 
-                        <a class="dropdown-item" href="{$url}/main/category/?category_id={$category.id}">
+                        <a class="dropdown-item header-link" href="{$url}/main/category/?category_id={$category.id}">
                             <i class="fa fa-{$category.icon} header__icon" aria-hidden="true"></i> {$category.name}
                         </a>
 
@@ -36,7 +36,7 @@
                 </div>
                 <div class="d-block d-md-none header__categories dropdown">
                     <i class="header__icon fa fa-bars" aria-hidden="true"></i>
-                    <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <a class="header-link" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                         Категории
                     </a>
                 </div>
@@ -49,10 +49,19 @@
                 </div>
             </div>
             <div class="d-none d-md-flex col-2 col-m-3 col-md-2 order-4">
-                <div class="header__contacts">Контакты</div>
+                <div class="header__order">
+                    <i class="header__icon fa fa-shopping-cart" aria-hidden="true">
+                        <span class="header__order-count">2</span>
+                    </i>
+                    <a href="#" class="header-link">Корзина</a>
+                </div>
             </div>
             <div class="d-none d-md-flex col-2 col-m-3 col-md-2 order-5">
-                <div class="header__help">Помощь</div>
+                <div class="header__account">
+                    <a href="#" class="standart-btn" data-toggle="modal" data-target="#signinModal">
+                        Войти
+                    </a>
+                </div>
             </div>
         </div>
         <div class="row">
@@ -66,14 +75,6 @@
                     </a>
 
                     {/foreach}
-                    
-                    <hr class="header__categories-collapse-seperator">
-                    <a class="dropdown-item header__categories-collapse-item" href="#">
-                        Контакты
-                    </a>
-                    <a class="dropdown-item header__categories-collapse-item" href="#">
-                        Помощь
-                    </a>
                 </div>
             </div>
         </div>
@@ -84,6 +85,54 @@
     <footer class="container">
         Footer
     </footer>
+
+    <div class="modal fade" id="signinModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm">
+          <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Авторизация</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="modal__form">
+                    <input type="text" name="email" placeholder="Введите ваш Email">
+                    <input type="password" name="password" placeholder="Введите ваш пароль">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="modal-btn standart-btn">Войти</a>
+                <a href="#" class="login-modal__signup" data-dismiss="modal"  data-toggle="modal" data-target="#signupModal">Зарегистрироваться</a>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="signupModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-sm">
+          <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Регистрация</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="modal__form">
+                    <input type="text" name="name" placeholder="Имя">
+                    <input type="text" name="email" placeholder="Введите ваш Email">
+                    <input type="password" name="password1" placeholder="Пароль">
+                    <input type="password" name="password2" placeholder="Пароль повторно">
+                </form>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="modal-btn standart-btn">Создать аккаунт</a>
+            </div>
+          </div>
+        </div>
+    </div>
+    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
